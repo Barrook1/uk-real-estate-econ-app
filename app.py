49da279aa48c52f66835a8de8496817ea348a486
Barrook1 <mubarak.z@hotmail.com>
2026-04-28 10:41:41 +0300

    app.py

--- app.py
+++ app.py
@@ -284,7 +284,7 @@ if run:
     house = get_house_price_england()
     bank = get_bank_rate()
     guard = get_guardian_sentiment(query, str(from_date), str(to_date))
-
+     except Exception:
         rows = data.get("rows", [])
     epc_size = get_epc_size_for_postcode(postcode)
 
